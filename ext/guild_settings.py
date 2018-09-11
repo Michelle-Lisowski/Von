ee6a7e2bfd3160ecac26bb7837a4818e43179db4
Sir Tezza <sirtezza451@outlook.com>
2018-09-11 18:17:11 +0930

    Fix owner-based checks

--- ext/guild_settings.py
+++ ext/guild_settings.py
@@ -34,9 +34,6 @@ class GuildSettings:
         elif isinstance(error, commands.CheckFailure):
             await ctx.send(':no_entry_sign: You must be the owner of this server to use this command.')
 
-        elif isinstance(error, MissingPermissions):
-            await ctx.send(':no_entry_sign: You require the `Staff` role to use this command.')
-
         elif isinstance(error, commands.BadArgument):
             await ctx.send(':x: Please specify a **whole number** for the new default volume.')
 
@@ -62,26 +59,23 @@ class GuildSettings:
         if staff_role is None:
             staff_role = await ctx.guild.create_role(name='Staff', colour=role_colour, hoist=True, reason='Role for server staff/moderators.')
 
-        if not staff_role in ctx.author.roles:
-            raise MissingPermissions
-        else:
-            if str(ctx.guild.id) in guilds:
-                if new_prefix is None:
-                    await ctx.send(f":information_source: Current server prefix: **{guilds[str(ctx.guild.id)]['GUILD_PREFIX']}**")
-                else:
-                    del guilds[str(ctx.guild.id)]['GUILD_PREFIX']
-                    guilds[str(ctx.guild.id)]['GUILD_PREFIX'] = new_prefix
-                    await ctx.send(f":information_source: New server prefix: **{new_prefix}**")
+        if str(ctx.guild.id) in guilds:
+            if new_prefix is None:
+                await ctx.send(f":information_source: Current server prefix: **{guilds[str(ctx.guild.id)]['GUILD_PREFIX']}**")
             else:
-                if new_prefix is None:
-                    await ctx.send(':information_source: Current server prefix: **.**')
-                else:
-                    guilds[str(ctx.guild.id)] = {}
-                    guilds[str(ctx.guild.id)]['GUILD_PREFIX'] = new_prefix
-                    await ctx.send(f":information_source: New server prefix: **{new_prefix}**")
+                del guilds[str(ctx.guild.id)]['GUILD_PREFIX']
+                guilds[str(ctx.guild.id)]['GUILD_PREFIX'] = new_prefix
+                await ctx.send(f":information_source: New server prefix: **{new_prefix}**")
+        else:
+            if new_prefix is None:
+                await ctx.send(':information_source: Current server prefix: **.**')
+            else:
+                guilds[str(ctx.guild.id)] = {}
+                guilds[str(ctx.guild.id)]['GUILD_PREFIX'] = new_prefix
+                await ctx.send(f":information_source: New server prefix: **{new_prefix}**")
 
-            with open('guilds.json', 'w') as fp:
-                json.dump(guilds, fp, indent=4)
+        with open('guilds.json', 'w') as fp:
+            json.dump(guilds, fp, indent=4)
 
     @setting.command()
     @commands.guild_only()
@@ -95,26 +89,23 @@ class GuildSettings:
         if staff_role is None:
             staff_role = await ctx.guild.create_role(name='Staff', colour=role_colour, hoist=True, reason='Role for server staff/moderators.')
 
-        if not staff_role in ctx.author.roles:
-            raise MissingPermissions
-        else:
-            if str(ctx.guild.id) in guilds:
-                if new_volume is None:
-                    await ctx.send(f":information_source: Current default volume: **{round(guilds[str(ctx.guild.id)]['DEFAULT_VOLUME'] * 100)}%**")
-                else:
-                    del guilds[str(ctx.guild.id)]['DEFAULT_VOLUME']
-                    guilds[str(ctx.guild.id)]['DEFAULT_VOLUME'] = (new_volume / 100)
-                    await ctx.send(f":information_source: New default volume: **{new_volume}%**")
+        if str(ctx.guild.id) in guilds:
+            if new_volume is None:
+                await ctx.send(f":information_source: Current default volume: **{round(guilds[str(ctx.guild.id)]['DEFAULT_VOLUME'] * 100)}%**")
             else:
-                if new_volume is None:
-                    await ctx.send(":information_source: Current default volume: **50%**")
-                else:
-                    guilds[str(ctx.guild.id)] = {}
-                    guilds[str(ctx.guild.id)]['DEFAULT_VOLUME'] = (new_volume / 100)
-                    await ctx.send(f":information_source: New default volume: **{new_volume}%**")
+                del guilds[str(ctx.guild.id)]['DEFAULT_VOLUME']
+                guilds[str(ctx.guild.id)]['DEFAULT_VOLUME'] = (new_volume / 100)
+                await ctx.send(f":information_source: New default volume: **{new_volume}%**")
+        else:
+            if new_volume is None:
+                await ctx.send(":information_source: Current default volume: **50%**")
+            else:
+                guilds[str(ctx.guild.id)] = {}
+                guilds[str(ctx.guild.id)]['DEFAULT_VOLUME'] = (new_volume / 100)
+                await ctx.send(f":information_source: New default volume: **{new_volume}%**")
 
-            with open('guilds.json', 'w') as fp:
-                json.dump(guilds, fp, indent=4)
+        with open('guilds.json', 'w') as fp:
+            json.dump(guilds, fp, indent=4)
 
 def setup(bot):
     bot.add_cog(GuildSettings(bot))
