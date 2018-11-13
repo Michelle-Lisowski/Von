590895982c2555d21aa189d61f806994478e86e4
Sir Tezza <sirtezza451@outlook.com>
2018-11-13 15:57:07 +1030

    Allow for different token without config change

--- launcher.py
+++ launcher.py
@@ -2,6 +2,7 @@
 # Copyright (c) 2018 sirtezza451
 # -*- coding: utf-8 -*-
 
+import argparse
 import json
 import sys
 
@@ -10,33 +11,48 @@ from discord.ext import commands
 
 from main import Von
 
+parser = argparse.ArgumentParser()
+parser.add_argument(
+    "-t",
+    "--token",
+    help="[temporary] Run Von with a token other than the token set.",
+    type=str,
+)
+args = parser.parse_args()
+
 
 def user_input():
-    return input(">>> ")
+    try:
+        return input(">>> ")
+    except EOFError:
+        sys.exit(0)
 
 
 def get_token():
-    try:
-        with open("config.json") as f:
-            try:
-                config = json.load(f)
-            except:
-                print("Configuration file doesn't contain a dictionary. Exiting.")
-                sys.exit(1)
-    except FileNotFoundError:
-        print("Configuration file doesn't exist. Exiting.")
-        sys.exit(1)
+    if args.token is not None:
+        return args.token
+    else:
+        try:
+            with open("config.json") as f:
+                try:
+                    config = json.load(f)
+                except:
+                    print("Configuration file doesn't contain a dictionary. Exiting.")
+                    sys.exit(1)
+        except FileNotFoundError:
+            print("Configuration file doesn't exist. Exiting.")
+            sys.exit(1)
 
-    try:
-        token = config["token"]
-    except KeyError:
-        print("Enter your Discord token below.")
-        config["token"] = user_input()
-        token = config["token"]
+        try:
+            token = config["token"]
+        except KeyError:
+            print("Enter your Discord token below.")
+            config["token"] = user_input()
+            token = config["token"]
 
-    with open("config.json", "w") as f:
-        json.dump(config, f, indent=4)
-    return token
+        with open("config.json", "w") as f:
+            json.dump(config, f, indent=4)
+        return token
 
 
 def main(token):
