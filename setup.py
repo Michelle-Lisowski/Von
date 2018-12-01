29c30de326a89ee17981c3307f52008841d1e38c
Sir Tezza <sirtezza451@outlook.com>
2018-12-01 18:21:55 +1030

    Update setup and add option to update bot

--- setup.py
+++ setup.py
@@ -6,6 +6,11 @@ import argparse
 import json
 import subprocess
 import sys
+from json.decoder import JSONDecodeError
+from subprocess import CalledProcessError
+from os.path import isdir
+
+from main import Von
 
 print("Checking Python version...")
 
@@ -13,12 +18,14 @@ if not sys.version_info >= (3, 6, 2):
     print("Python 3.6.2 or above is required. Please update Python and try again.")
     sys.exit(1)
 
-from json.decoder import JSONDecodeError
-from main import Von
-
 
 parser = argparse.ArgumentParser()
-parser.add_argument("-u", "--upgrade", help="upgrade dependencies", action="store_true")
+parser.add_argument(
+    "-u", "--upgrade-deps", help="upgrade dependencies", action="store_true"
+)
+parser.add_argument(
+    "-p", "--update-bot", help="pull code from git repository", action="store_true"
+)
 args = parser.parse_args()
 
 
@@ -30,6 +37,13 @@ def is_venv():
     return hasattr(sys, "real_prefix") or (
         hasattr(sys, "base_prefix") and sys.base_prefix != sys.prefix
     )
+
+
+# def yes_no_input(msg):
+    # print(f"{msg} [y/n]")
+    # i = input(">>>")
+
+    # if i.lower() in ["yes", "y"]
 
 
 def load_config():
@@ -84,11 +98,57 @@ def check_db():
 
 def install_dependencies():
     arguments = [sys.executable, "-m", "pip", "install", "-r", "requirements.txt"]
-    if args.upgrade:
+    if args.upgrade_deps:
         arguments.insert(4, "--upgrade")
     if not is_venv():
         arguments.insert(4, "--user")
-    subprocess.call(arguments)
+    command = " ".join(arguments)
+
+    try:
+        subprocess.check_call(command, shell=True)
+    except CalledProcessError:
+        print("Dependency installation failed.")
+        sys.exit(1)
+
+
+def update_bot():
+    print("Checking for Git Bash...")
+    if not isdir(".git"):
+        print(
+            "You have not cloned Von from the repository using Git.",
+            "This means it is not possible to update Von from the command line.",
+            sep="\n",
+        )
+        sys.exit(1)
+
+    try:
+        subprocess.check_call("git --version", shell=True)
+    except CalledProcessError:
+        print(
+            "Make sure Git Bash is installed on your system.",
+            "If it is, make sure you have set it up to run from the command line.",
+            sep="\n",
+        )
+        sys.exit(1)
+
+    print("Passed Git Bash checks...")
+
+    print(
+        "If you have made any modifications to the source code yourself, you will",
+        "lose them by updating. Are you sure you want to update? [y/n]",
+    )
+
+    yn = input(">>> ")
+
+    if yn.lower() in ["yes", "y"]:
+        try:
+            subprocess.check_call("git pull", shell=True)
+        except CalledProcessError:
+            print("Updating Von failed. Try running 'git pull' yourself.")
+            sys.exit(1)
+    else:
+        print("Skipping update...")
+        return
 
 
 def main():
@@ -110,11 +170,11 @@ def main():
     check_db()
 
     print("Installing dependencies...")
-    try:
-        install_dependencies()
-    except:
-        print("Dependency installation failed.")
-        sys.exit(1)
+    install_dependencies()
+
+    if args.update_bot:
+        print("Updating bot...")
+        update_bot()
 
     print("Setup complete.")
     sys.exit(0)
