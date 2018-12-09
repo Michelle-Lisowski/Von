23f0e3396b493c528856ba666ca1cd0dd0622bbc
Sir Tezza <sirtezza451@outlook.com>
2018-12-09 16:20:42 +1030

    Add dependency installation to setup

--- setup.py
+++ setup.py
@@ -1,7 +1,10 @@
 # -*- coding: utf-8 -*-
 
 import argparse
+import subprocess
 import sys
+from os.path import isdir
+from subprocess import CalledProcessError
 
 try:
     from bot import Von
@@ -11,7 +14,30 @@ except (ImportError, SyntaxError):
 
 parser = argparse.ArgumentParser()
 parser.add_argument("-r", "--run", help="run bot", action="store_true")
+parser.add_argument("-u", "--upgrade", help="upgrade dependencies", action="store_true")
 args = parser.parse_args()
+
+
+def is_venv():
+    return hasattr(sys, "real_prefix") or (
+        hasattr(sys, "base_prefix") and sys.base_prefix != sys.prefix
+    )
+
+
+def install_dependencies():
+    arguments = [sys.executable, "-m", "pip", "install", "-r", "requirements.txt"]
+
+    if args.upgrade:
+        arguments.insert(4, "--upgrade")
+
+    if not is_venv():
+        arguments.insert(4, "--user")
+
+    try:
+        command = " ".join(arguments)
+        subprocess.check_call(command, shell=True)
+    except CalledProcessError:
+        print("Dependency installation failed.")
 
 
 def main():
@@ -19,8 +45,12 @@ def main():
         print("Python 3.6 or above is required to run Von.")
         sys.exit(1)
 
+    print("Installing dependencies...")
+    install_dependencies()
+
     print("Setup complete.")
     if args.run:
+        print("Starting bot...")
         Von().run()
     else:
         sys.exit(0)
