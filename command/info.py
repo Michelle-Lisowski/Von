dcf1fc51378f7adad106d5975ff210e69e9042b9
Sir Tezza <sirtezza451@outlook.com>
2018-12-10 21:23:10 +1030

    Basic info command (memory usage, CPU usage, thread count)

---
+++ command/info.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+
+import discord
+from discord.ext import commands
+import sys
+
+
+@commands.command()
+async def info(ctx):
+    embed = discord.Embed()
+    embed.colour = 0x0099FF
+    embed.title = ctx.bot.user.name
+    info = ctx.bot.process.as_dict(attrs=["cpu_percent", "num_threads", "memory_info"])
+
+    embed.add_field(
+        name="Memory Usage", value=f"{info['memory_info']._asdict()['vms'] >> 20} MB"
+    )
+    embed.add_field(name="CPU Usage", value=f"{round(info['cpu_percent'], 1)}%")
+    embed.add_field(name="Thread Count", value=f"{info['num_threads']} Threads")
+    await ctx.send(embed=embed)
+
+
+def setup(bot):
+    bot.add_command(info)
