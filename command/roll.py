162d29117ff5cf7d025470d678028323936f6cc0
Sir Tezza <sirtezza451@outlook.com>
2018-12-15 21:54:35 +1030

    Die roll command

---
+++ command/roll.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+
+import random
+import typing
+
+import discord
+from discord.ext import commands
+
+
+@commands.command()
+async def roll(ctx, minimum: int = None, maximum: int = None):
+    if maximum is None and minimum is not None:
+        maximum = minimum
+        minimum = 1
+    elif minimum is None and maximum is None:
+        minimum = 1
+        maximum = 6
+
+    if minimum > maximum:
+        minimum, maximum = maximum, minimum
+
+    result = random.randint(minimum, maximum)
+    message = await ctx.send(":game_die: The die rolls...")
+
+    cache = await ctx.get_message(message.id)
+    await cache.edit(content=f":game_die: The die rolls: **{result}**")
+
+
+def setup(bot):
+    bot.add_command(roll)
