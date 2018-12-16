cf9354dde9b10a1801497a702a12fe3194fbf8cb
Sir Tezza <sirtezza451@outlook.com>
2018-12-16 17:21:46 +1030

    Make use of Playlist class in get_player function

--- utils/audio.py
+++ utils/audio.py
@@ -1,10 +1,12 @@
 # -*- coding: utf-8 -*-
 
+from .playlist import Playlist
+
 
 def get_player(ctx):
     try:
         player = ctx.bot.players[str(ctx.guild.id)]
     except KeyError:
-        ctx.bot.players[str(ctx.guild.id)] = None
+        ctx.bot.players[str(ctx.guild.id)] = Playlist()
         player = ctx.bot.players[str(ctx.guild.id)]
     return player
