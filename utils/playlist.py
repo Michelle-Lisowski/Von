114b8e9fd5856228d19bd6d4c822b723836d9d9f
Sir Tezza <sirtezza451@outlook.com>
2018-12-18 18:51:41 +1030

    Basic playlist functionality

--- utils/playlist.py
+++ utils/playlist.py
@@ -10,7 +10,40 @@ class Playlist:
         self.ctx = ctx
 
         self.song = None
-        self.loop = ctx.bot.loop.create_task(self.play())
+        self.loop = ctx.bot.loop.create_task(self.playlist())
+
+    def clear(self):
+        self.queue._queue.clear()
+
+    async def add(self, source):
+        await self.queue.put(source)
 
     async def play(self):
-        pass
+        self.ctx.voice_client.play(
+            self.song,
+            after=lambda _: self.ctx.bot.loop.call_soon_threadsafe(self.next.set),
+        )
+
+        await self.ctx.channel.send(
+            ":musical_note: Now playing: **{0.uploader}** - **{0.title}**.".format(
+                self.song
+            )
+        )
+
+    async def playlist(self):
+        await self.ctx.bot.wait_until_ready()
+        while not self.ctx.bot.is_closed():
+            self.next.clear()
+
+            source = await self.queue.get()
+            self.song = source
+
+            await self.play()
+            await self.next.wait()
+
+            source.cleanup()
+            self.song = None
+
+            if len(self.queue._queue) == 0:
+                await self.ctx.voice_client.disconnect()
+                del self.ctx.bot.players[str(self.ctx.guild.id)]
